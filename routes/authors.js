ff2accf7469826767efc0cbaf2cc39ff00332cfb
Jonathan Casey <jon_casey@me.com>
2016-01-17 20:13:21 -0700

    add authors view/routes

--- routes/authors.js
+++ routes/authors.js
@@ -1,9 +1,33 @@
 var express = require('express');
 var router = express.Router();
+var knex = require('../db/knex');
 
+function combineBooksAuthors(books, authors){
+  authors.map(function(author){
+    author.books = [];
+    books.map(function(book){
+      if (author.id === book.author_id){
+        author.books.push(book.title);
+      }else{
+        return;
+      }
+    });
+    author.bookList = author.books.join('\n');
+  });
+  return authors;
+}
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.send('authors');
+  var authors;
+  var books;
+  knex('authors').then(function(data){
+    authors = data;
+    knex('books').join('books_authors', {'books_authors.book_id' : 'books.id'}).then(function(data){
+      books = data;
+      var list = combineBooksAuthors(books, authors);
+      res.render('authors', {authors: list});
+    });
+  });
 });
 
 module.exports = router;
